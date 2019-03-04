f90d741da2c7a05ac6e3c052f079cb6ae6b9486c
Kim Streicher <kim.streicher@allata.com>
2019-03-04 13:35:46 -0700

    class binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <div class="jumbotron">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div>Time: {{ event?.time }}</div>
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{ event?.price }}</div>
       <div *ngIf="event?.location">
         <span>Location: {{ event?.location.address }}</span>
@@ -26,10 +31,21 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
       .jumbotron {
         min-height: 210px;
       }
+      .green {
+        color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
     `
   ]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
   someProperty: any = "some value";
+
+  getStartTimeClass() {
+    if (this.event && this.event.time === "8:00 am") return "green bold";
+    return "";
+  }
 }
