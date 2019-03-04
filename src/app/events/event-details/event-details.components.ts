804560f62c74dd9ec10585a69c0e286d84077120
Kim Streicher <kim.streicher@allata.com>
2019-03-04 15:52:01 -0700

    routes added

--- src/app/events/event-details/event-details.components.ts
+++ src/app/events/event-details/event-details.components.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { EventService } from "../shared/events.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   templateUrl: "/event-details.component.html",
   styles: [
     `
-      .event-image: {
+      .event-image {
         height: 100px;
       }
     `
@@ -13,10 +14,12 @@ import { EventService } from "../shared/events.service";
 })
 export class EventDetailsComponent {
   event: any;
-  constructor(private eventService: EventService) {}
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    debugger;
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(+this.route.snapshot.params["id"]);
   }
 }
