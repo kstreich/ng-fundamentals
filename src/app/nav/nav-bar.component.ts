36836c92de181b899b976e39f86dd04ee6ec3723
Kim Streicher <kim.streicher@allata.com>
2019-03-07 17:26:45 -0700

    fixed changes in the dependencies

--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -2,6 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "nav-bar",
-  templateUrl: "./nav-bar.component.html"
+  templateUrl: "./nav-bar.component.html",
+  styles: [
+    `
+      .nav.nav-bar {
+        font-size: 15px;
+      }
+      li > a.active {
+        color: #f97924;
+      }
+    `
+  ]
 })
 export class NavBarComponent {}
