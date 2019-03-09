22feeb3b5070aba63bc2d81da8ffcfe334e4829c
Kim Streicher <kim.streicher@allata.com>
2019-03-09 14:33:23 -0700

    user module and barrel loaders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from "./events/index";
 import { EventsAppComponent } from "./events-app.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/nav-bar.component";
 import { EventService } from "./events/shared/events.service";
 import { ToastrService } from "./common/toastr.service";
-import { EventDetailsComponent } from "./events/event-details/event-details.components";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./routes";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activators.service";
-import { EventListResolver } from "./events/events-list-resolver.service";
 
 @NgModule({
   declarations: [
---
+++ src/app/events/event-details/index.ts
@@ -0,0 +1,2 @@
+export * from "./event-route-activators.service";
+export * from "./event-details.components";
